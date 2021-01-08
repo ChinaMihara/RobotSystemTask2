dd6a626c074a0ae19c383e68c84ef65378fe2482
China Mihara <72910627+ChinaMihara@users.noreply.github.com>
2021-01-08 14:41:26 +0900

    Update led_sub.py

--- scripts/led_sub.py
+++ scripts/led_sub.py
@@ -3,31 +3,31 @@
 import rospy
 from std_msgs.msg import Bool
 
-def led1_callback(msg):
+def led_red_callback(msg):
     with open("/dev/myled0", "w") as f:
         f.write("1\n" if msg.data else "0\n")
 
-def led2_callback(msg):
+def led_yellow_callback(msg):
     with open("/dev/myled0", "w") as f:
         f.write("3\n" if msg.data else "2\n")
 
-def led3_callback(msg):
+def led_green_callback(msg):
     with open("/dev/myled0", "w") as f:
         f.write("5\n" if msg.data else "4\n")
 
-def led4_callback(msg):
+def led_blue_callback(msg):
     with open("/dev/myled0", "w") as f:
         f.write("7\n" if msg.data else "6\n")
 
-def led5_callback(msg):
+def led_white_callback(msg):
     with open("/dev/myled0", "w") as f:
         f.write("9\n" if msg.data else "8\n")
 
 if __name__ == "__main__":
     rospy.init_node("led_flusher")
-    sub1 = rospy.Subscriber("led1", Bool, led1_callback, queue_size=10)
-    sub2 = rospy.Subscriber("led2", Bool, led2_callback, queue_size=10)
-    sub3 = rospy.Subscriber("led3", Bool, led3_callback, queue_size=10)
-    sub4 = rospy.Subscriber("led4", Bool, led4_callback, queue_size=10)
-    sub5 = rospy.Subscriber("led5", Bool, led5_callback, queue_size=10)
+    sub1 = rospy.Subscriber("led_red", Bool, led1_callback, queue_size=10)
+    sub2 = rospy.Subscriber("led_yellow", Bool, led2_callback, queue_size=10)
+    sub3 = rospy.Subscriber("led_green", Bool, led3_callback, queue_size=10)
+    sub4 = rospy.Subscriber("led_blue", Bool, led4_callback, queue_size=10)
+    sub5 = rospy.Subscriber("led_white", Bool, led5_callback, queue_size=10)
     rospy.spin()
